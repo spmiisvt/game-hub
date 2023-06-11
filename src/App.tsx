ba8a4ebe0d65146d2c3886daf63a0e19fef64ba4
webency <spmiisvt@gmail.com>
2023-06-11 12:16:11 +0300

    simplify game query

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import {useState} from "react";
-import {Genre} from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
-import {Platform} from "./hooks/usePlatforms";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
-    genre: Genre | null;
-    platform: Platform | null;
+    genreId?: number;
+    platformId?: number;
     sortOrder: string;
     searchText: string;
 }
@@ -37,8 +35,9 @@ function App() {
             <Show above='lg'>
                 <GridItem area='aside' paddingX={5}>
                     <GenreList
-                        selectedGenre={gameQuery.genre}
-                        onSelectGenre={(genre) => setGameQuery({...gameQuery, genre})}/>
+                        selectedGenreId={gameQuery.genreId}
+                        onSelectGenre={(genre) =>
+                            setGameQuery({...gameQuery, genreId: genre.id})}/>
                 </GridItem>
             </Show>
             <GridItem area='main'>
@@ -47,12 +46,14 @@ function App() {
                     <Flex marginBottom={5}>
                         <Box marginRight={5}>
                             <PlatformSelector
-                                selectedPlatform={gameQuery.platform}
-                                onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}
+                                selectedPlatformId={gameQuery.platformId}
+                                onSelectPlatform={(platform) =>
+                                    setGameQuery({...gameQuery, platformId: platform.id})}
                             />
                         </Box>
                         <SortSelector sortOrder={gameQuery.sortOrder}
-                                      onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
+                                      onSelectSortOrder={(sortOrder) =>
+                                          setGameQuery({...gameQuery, sortOrder})}/>
                     </Flex>
                 </Box>
                 <GameGrid
