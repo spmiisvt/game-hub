b0ecb6e68bccc3a6289ecce380f41b1f2e46568d
webency <spmiisvt@gmail.com>
2023-06-12 01:37:53 +0300

    Fetch game details

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -4,9 +4,16 @@ import {Box} from "@chakra-ui/react";
 interface Props {
     children: ReactNode;
 }
-const GameCardContainer = ({children} : Props) => {
+
+const GameCardContainer = ({children}: Props) => {
     return (
-        <Box borderRadius={10} overflow='hidden'>
+        <Box
+            _hover={{
+                transform: 'scale(1.03)',
+                transition: 'transform .15s ease-in'
+            }}
+            borderRadius={10}
+            overflow='hidden'>
             {children}
         </Box>
     );
