fff7aa31ff230e8639b4f2e5de48c80a2f86e5d9
webency <spmiisvt@gmail.com>
2023-06-11 11:54:58 +0300

    implement infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import {Box, Button, SimpleGrid, Text} from "@chakra-ui/react";
+import {SimpleGrid, Spinner, Text} from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import {GameQuery} from "../App";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
     gameQuery: GameQuery;
@@ -16,7 +17,6 @@ const GameGrid = ({gameQuery}: Props) => {
         data,
         error,
         isLoading,
-        isFetchingNextPage,
         fetchNextPage,
         hasNextPage
     } = useGames(gameQuery)
@@ -24,10 +24,19 @@ const GameGrid = ({gameQuery}: Props) => {
 
     if (error) return <Text>{error.message}</Text>;
 
+    const fetchedGamesCount = data?.pages.reduce(
+        (total, page) => total + page.results.length, 0) || 0;
+
     return (
-        <Box padding='10px'>
+
+        <InfiniteScroll
+            next={() => fetchNextPage()}
+            hasMore={!!hasNextPage}
+            loader={<Spinner/>}
+            dataLength={fetchedGamesCount}>
             <SimpleGrid
                 columns={{sm: 1, md: 2, lg: 3, xl: 4}}
+                padding='10px'
                 spacing={6}
             >
 
@@ -47,12 +56,8 @@ const GameGrid = ({gameQuery}: Props) => {
                     </React.Fragment>
                 ))}
             </SimpleGrid>
-            {hasNextPage && (
-                <Button onClick={() => fetchNextPage()} marginY={5}>
-                    {isFetchingNextPage ? "Loading..." : "Load More"}
-                </Button>
-            )}
-        </Box>
+        </InfiniteScroll>
+
     );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
 import {GameQuery} from "../App";
-import {useInfiniteQuery, useQuery} from "@tanstack/react-query";
+import {useInfiniteQuery} from "@tanstack/react-query";
 import APIClient, {FetchResponse} from "../services/api-client";
 import {Platform} from "./usePlatforms";
 
@@ -28,7 +28,9 @@ const useGames = (gameQuery: GameQuery) =>
         }),
         getNextPageParam: (lastPage, allPages) => {
             return lastPage.next ? allPages.length + 1 : undefined;
-        }
+        },
+        staleTime: 24 * 60 * 60 * 1000, //24h
+
     })
 
 
