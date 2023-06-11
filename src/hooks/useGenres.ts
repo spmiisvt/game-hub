01453815ed55f8cdf0bb95909575610273455607
webency <spmiisvt@gmail.com>
2023-06-12 01:44:46 +0300

    Refactor -> move entity interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,14 +1,10 @@
 import genres from "../data/genres";
 import {useQuery} from "@tanstack/react-query";
-import APIClient  from "../services/api-client";
+import APIClient from "../services/api-client";
 import ms from 'ms';
-const apiClient = new APIClient<Genre>('/genres')
+import {Genre} from "../entities/Genre";
 
-export interface Genre {
-    id: number;
-    name: string;
-    image_background: string;
-}
+const apiClient = new APIClient<Genre>('/genres')
 
 const useGenres = () => useQuery({
     queryKey: ['genres'],
