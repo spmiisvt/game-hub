86391df50722c2f842d7f8fedb22cfb96db45ece
webency <spmiisvt@gmail.com>
2023-06-12 03:00:08 +0300

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
 import useGame from "../hooks/useGame";
-import {Heading, Spinner} from "@chakra-ui/react";
+import {GridItem, Heading, SimpleGrid, Spinner} from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetailPage = () => {
     const {slug} = useParams();
@@ -16,12 +17,17 @@ const GameDetailPage = () => {
     if (error || !game) throw error;
 
     return (
-        <>
-            <Heading>{game.name}</Heading>
-            <ExpandableText>{game.description_raw}</ExpandableText>
-            <GameAttributes game={game}/>
-            <GameTrailer gameId={game.id} />
-        </>
+        <SimpleGrid columns={{base: 1, md: 2}} spacing={5}>
+            <GridItem>
+                <Heading>{game.name}</Heading>
+                <ExpandableText>{game.description_raw}</ExpandableText>
+                <GameAttributes game={game}/>
+            </GridItem>
+            <GridItem>
+                <GameTrailer gameId={game.id}/>
+                <GameScreenshots gameId={game.id}/>
+            </GridItem>
+        </SimpleGrid>
     );
 };
 
